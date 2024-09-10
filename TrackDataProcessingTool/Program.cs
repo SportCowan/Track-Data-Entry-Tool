a6a2c749a203df91b7c2ac5f3a76e32dd2faf69b
SportCowan <speeedyman@gmail.com>
2024-09-09 17:02:07 -0700

    initial commit

---
+++ TrackDataProcessingTool/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Uploading Workout");
+//put new workout into sql database
+Console.WriteLine("Integrating Workout");
+//figure out important stats from workout
+Console.WriteLine("Evaluating Data");
+//update pr's/other important info
